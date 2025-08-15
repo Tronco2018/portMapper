2688ccfc39d816f74de11057f275dd35228cae50
tronco2018 <tronco2018dev@gmail.com>
2025-08-16 01:32:36 +0200

    Added basic interface and elapsed time

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,15 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+#include <chrono>
+
+long long currentTimeMillis(){
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(
+    system_clock::now().time_since_epoch()
+  ).count();
+}
+
 bool isPortOpen(const std::string& host, int port){
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0){
@@ -28,9 +37,21 @@ bool isPortOpen(const std::string& host, int port){
 }
 
 int main(int argc, char* argv[]){
+  int lastPort = 9999;
+  int portCount = 0;
   if (argc < 2){
     std::cout << "Usage: portmap <192.168.x.x>";
     return 0;
   }
-
+  std::string ip = argv[1];
+  long current = currentTimeMillis();
+  for (int i = 0; i <= lastPort; i++){
+    if (isPortOpen(ip, i)){
+      std::cout << "[+] " << i << " port open\n";
+      portCount++;
+    }
+  }
+  long elapsedMs = currentTimeMillis() - current;
+  std::cout << "Finished in " << elapsedMs/1000 << "s, " << portCount << " open ports found";
+  return 0;
 }
